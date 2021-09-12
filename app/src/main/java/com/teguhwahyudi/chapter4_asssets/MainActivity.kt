317493910de11ee7fac696b42b66b6e5c57c55bc
t39uh <teguh.wahyu@gmail.com>
2021-09-12 21:49:08 +0700

    finish

---
+++ app/src/main/java/com/teguhwahyudi/chapter4_asssets/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.teguhwahyudi.chapter4_asssets
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import java.io.IOException
+
+const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var imageView = findViewById<ImageView>(R.id.ivRocket)
+        val fileName = "rocket.png"
+        imageView.setImageAssets(applicationContext,fileName)
+    }
+}
+
+fun ImageView.setImageAssets(context: Context, fileName: String){
+    try {
+        with(context.assets.open(fileName)){
+            setImageBitmap(BitmapFactory.decodeStream(this))
+        }
+    }catch (e: IOException) {
+        Log.e(TAG, e.toString())
+    }
+}
